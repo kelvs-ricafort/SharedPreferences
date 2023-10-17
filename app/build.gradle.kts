143289437eac3f003d97bf98a5b0bfeb609e2207
Kelvin Ricafort <kelvs.r@outlook.com>
2023-10-17 14:21:12 +0800

    Calling back data

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.newtechieblog.wordpress.views.sharedpreferences"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.newtechieblog.wordpress.views.sharedpreferences"
--- app/src/main/java/com/newtechieblog/wordpress/views/sharedpreferences/MainActivity.java
+++ app/src/main/java/com/newtechieblog/wordpress/views/sharedpreferences/MainActivity.java
@@ -2,13 +2,88 @@ package com.newtechieblog.wordpress.views.sharedpreferences;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText userName;
+    EditText userMessage;
+    Button counter;
+    CheckBox remember;
+    int count = 0;
+    String name;
+    String message;
+    boolean isChecked;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userName = findViewById(R.id.editTextName);
+        userMessage = findViewById(R.id.editTextMessage);
+        counter = findViewById(R.id.btnCounter);
+        remember = findViewById(R.id.checkBoxRemember);
+
+        retrieveData();
+
+        counter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count = count + 1;
+                counter.setText("" + count);
+            }
+        });
+    }
+
+    public void saveData() {
+        sharedPreferences = getSharedPreferences("saveData", Context.MODE_PRIVATE);
+        name = userName.getText().toString();
+        message = userMessage.getText().toString();
+        if (remember.isChecked()) {
+            isChecked = true;
+        } else {
+            isChecked = false;
+        }
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("key name", name);
+        editor.putString("key message", message);
+        editor.putInt("key count", count);
+        editor.putBoolean("key remember", isChecked);
+        editor.commit();
+
+        Toast.makeText(getApplicationContext(), "Your data is saved", Toast.LENGTH_LONG).show();
+    }
+
+    public void retrieveData() {
+        sharedPreferences = getSharedPreferences("saveData", MODE_PRIVATE);
+
+        name = sharedPreferences.getString("key name", null);
+        message = sharedPreferences.getString("key message", null);
+        count = sharedPreferences.getInt("key count", 0);
+        isChecked = sharedPreferences.getBoolean("key remember", false);
+
+        userName.setText(name);
+        userMessage.setText(message);
+        counter.setText("" + count);
+        if (isChecked) {
+            remember.setChecked(true);
+        } else {
+            remember.setChecked(false);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        saveData();
+        super.onPause();
     }
 }
